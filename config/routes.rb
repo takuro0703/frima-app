577890366c4ad5d847fc5e68d82f7b16a7fabb07
shouyoshi <s.y.5180@au.com>
2020-08-01 18:10:57 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,4 @@ Rails.application.routes.draw do
     post 'confirmation', to: 'users/registrations#create_confirmation'
   end
   root to: 'items#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
 end
