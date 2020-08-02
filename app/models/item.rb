7cf04c8cdaee41f7ee9cd828f58318267c0c08ca
toshi0805 <67369526+toshi0805@users.noreply.github.com>
2020-08-02 18:32:14 +0900

    コメント

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,5 +21,7 @@ class Item < ApplicationRecord
 
   has_many :likes
   has_many :liked_users, through: :likes, source: :user
+
+  has_many :comments
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_063232) do
+ActiveRecord::Schema.define(version: 2020_08_02_045507) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 2020_07_31_063232) do
     t.string "ancestry"
     t.string "name"
     t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "item_id"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "confirmations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
