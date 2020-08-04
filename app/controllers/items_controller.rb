49ec3d091ab3824d87e5f64709dc5b171f114aeb
takuro0703 <takuro0703dmwtp@icloud.com>
2020-08-04 17:28:01 +0900

    function for item-edit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :set_item,only: [:show, :destroy]
+  before_action :set_item,only: [:show, :edit, :update, :destroy]
+  before_action :show_all_instance, only: [:show, :edit, :update, :destroy]
+
 
   def index
     @items = Item.all
@@ -13,9 +15,9 @@ class ItemsController < ApplicationController
   end
 
   def create
-     @item = Item.new(item_params)
+    @item = Item.new(item_params)
    if @item.save
-     redirect_to root_path
+    redirect_to root_path
    else
     render :new
    end
@@ -24,10 +26,33 @@ class ItemsController < ApplicationController
   def show
     @comment = Comment.new
     @comments = @item.comments.includes(:user)
-   likes = @item.likes.map{|like| like.user_id == current_user.id}
+    likes = @item.likes.map{|like| like.user_id == current_user.id}
 
 
-     @category = Category.new
+    @category = Category.new
+  end
+
+  def edit
+  end
+
+  def update
+    if edit_item_params[:images_attributes].nil?
+      render :edit
+    else
+      exit_ids = []
+      edit_item_params[:images_attributes].each do |a,b|
+        exit_ids << edit_item_params[:images_attributes].dig(:"#{a}",:id).to_i
+      end
+      ids = Image.where(item_id: params[:id]).map{|image| image.id }
+      delete__db = ids - exit_ids
+      Image.where(id:delete__db).destroy_all
+      @item.touch
+      if @item.update(edit_item_params)
+        redirect_to  root_path
+      else
+        render :edit
+      end
+    end
   end
 
   def destroy
@@ -46,6 +71,17 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:item_name, :item_description, :category_id, :status_id, :brand_description, :fee_id, :prefecture_id, :span_id, :item_price, images_attributes: [:image]).merge(user_id: current_user.id, sold_status: "販売中")
+  end
+
+  def edit_item_params
+    params.require(:item).permit(:item_name, :item_description, :category_id, :status_id, :brand_description, :fee_id, :prefecture_id, :span_id, :item_price, images_attributes: [:image, :_destroy, :id]).merge(user_id: current_user.id, sold_status: "販売中")
+  end
+
+  def show_all_instance
+    @user = User.find(@item.user_id)
+    @images = Image.where(item_id: params[:id])
+    @images_first = Image.where(item_id: params[:id]).first
+    @category_id = @item.category_id
   end
 end
 
