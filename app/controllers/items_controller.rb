8fdac3443503ae16bdcafee7792f39d5f9930511
toshi0805 <67369526+toshi0805@users.noreply.github.com>
2020-08-05 16:59:24 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,18 +33,7 @@ class ItemsController < ApplicationController
 
   def search
     @items = Item.search(params[:keyword])
-    sort = params[:sort] || "created_at DESC"
-    # 入力された値をLIKE句により各カラムと一致したものを抽出する。
-    @items = Item.where('name LIKE(?) OR description LIKE(?)', "%#{@keyword}%", "%#{@keyword}%").order(sort)
-    # @count = @items.count
-    # 検索結果が"0"だった場合、全ての商品を表示させる
-    if @count == 0
-      @iems = Item.order(sort)
 
-    
-      end
-      
-  
     
   end
 
@@ -61,7 +50,14 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  private
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+
+
   def item_params
     params.require(:item).permit(:item_name, :item_description, :category_id, :status_id, :brand_description, :fee_id, :prefecture_id, :span_id, :item_price, images_attributes: [:image]).merge(user_id: current_user.id, sold_status: "販売中")
   end
