a1e3e04937b8ffeea455482f4ac513b6ad6539e2
shouyoshi <s.y.5180@au.com>
2020-08-09 17:06:26 +0900

    commit

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -23,7 +23,7 @@ $(function(){
 
   function appendGrandchildSelect(options){
     let select = `
-        <select class="grandchild_select select-box" name="item[category_id] id="item_category_id">
+        <select class="grandchild_select select-box" name="item[category_id]" id="item_category_id">
          <option value>選択してください</option>
          ${options}
         </select>
